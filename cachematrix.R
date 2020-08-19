2716708e1b234d4543df23fbd660523f29247f69
RiajuuAlbert <albert.setiawan98@hotmail.com>
2020-08-19 12:46:32 +0700

    Changed the message in Line 28

--- cachematrix.R
+++ cachematrix.R
@@ -25,7 +25,7 @@ cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
   invCache <- x$getInv()
   if(!is.null(invCache)) {        ## If there is an inverted matrix cache, the function will
-    message("getting cached data")## access the matrix from the cache to save time and 
+    message("Cache is being acquired, please wait...")## access the matrix from the cache to save time and 
     return(invCache)              ## processing power
   }
   data <- x$get()
